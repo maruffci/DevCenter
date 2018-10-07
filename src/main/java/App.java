93e02d16cd28d9dea8bf9e03db7b94bd0184708b
Md Maruf Billah <maruf.fci@gmail.com>
2018-10-07 13:22:00 +0200

    commiting after database connection

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,13 +6,47 @@ import org.eclipse.jetty.servlet.*;
 
 public class App extends HttpServlet {
 
+    private String API_KEY = "123456";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getWriter().print("Hello from Java built with Gradle!\n");
+        String api_key = req.getParameter("api_key");
+        if(api_key != null && !api_key.isEmpty()) {
+            resp.setHeader("Content-Type", "application/json;charset=utf-8");
+            if(api_key.equals(this.API_KEY)) {
+                String action = req.getParameter("action");
+                if (action.equals("create_developer")) {
+                    String email = req.getParameter("email");
+                    String plang = req.getParameter("plang");
+                    String lang = req.getParameter("lang");
+
+                    if ((email != null && !email.isEmpty() && !email.equals("")) && (plang != null && !plang.isEmpty() && !plang.equals("")) && (lang != null && !lang.isEmpty() && !lang.equals(""))) {
+                        try {
+                            Model m = new Model();
+                            String response = m.create_developer(email, plang, lang);
+                            resp.getWriter().print(response);
+                        } catch (Exception e) {
+                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database\"}]");
+                        }
+                    } else {
+                        resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid parameter list!\"}]");
+                    }
+                } else {
+                    resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid URL!\"}]");
+                }
+            }
+            else{
+                resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid API KEY\"}]");
+            }
+        }
+        else{
+            resp.getWriter().print("Welcome to Developer Center!");
+        }
     }
 
     public static void main(String[] args) throws Exception{
+
         int port = (System.getenv("PORT") == null) ? 80 : Integer.valueOf(System.getenv("PORT"));
         Server server = new Server(port);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
@@ -20,6 +54,6 @@ public class App extends HttpServlet {
         server.setHandler(context);
         context.addServlet(new ServletHolder(new App()),"/*");
         server.start();
-        server.join();   
+        server.join();
     }
 }
