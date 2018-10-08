d380728539dad8fa0a4a518d705923d4d98643d6
Md Maruf Billah <maruf.fci@gmail.com>
2018-10-08 12:47:25 +0200

    Initial commit for API completation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
@@ -16,10 +17,102 @@ public class App extends HttpServlet {
             resp.setHeader("Content-Type", "application/json;charset=utf-8");
             if(api_key.equals(this.API_KEY)) {
                 String action = req.getParameter("action");
-                if (action != null && !action.isEmpty() && action.equals("create_developer")) {
-                    String email = req.getParameter("email");
+                if (action != null && !action.isEmpty() && action.equals("retrieve_developer")) {
+                    int count = Integer.valueOf(req.getParameter("count"));
                     String plang = req.getParameter("plang");
                     String lang = req.getParameter("lang");
+                    if((lang != null && !lang.isEmpty()) && (plang != null && !plang.isEmpty()) && count > 0){
+                        try {
+                            Model m = new Model();
+                            String response = m.search_developer(lang, plang, count);
+                            resp.getWriter().print(response);
+                        } catch (Exception e) {
+                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database:"+e.getMessage()+"\"}]");
+                        }
+                    }
+                    else if((lang != null && !lang.isEmpty()) && count > 0){
+                        try {
+                            Model m = new Model();
+                            String response = m.search_lang_developer(lang, count);
+                            resp.getWriter().print(response);
+                        } catch (Exception e) {
+                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database:"+e.getMessage()+"\"}]");
+                        }
+                    }
+                    else if((plang != null && !plang.isEmpty()) && count > 0){
+                        try {
+                            Model m = new Model();
+                            String response = m.search_plang_developer(plang, count);
+                            resp.getWriter().print(response);
+                        } catch (Exception e) {
+                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database:"+e.getMessage()+"\"}]");
+                        }
+                    }
+                    else if (count > 0) {
+                        try {
+                            Model m = new Model();
+                            String response = m.retrieve_developer(count);
+                            resp.getWriter().print(response);
+                        } catch (Exception e) {
+                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database:"+e.getMessage()+"\"}]");
+                        }
+                    } else {
+                        resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid parameter list!\"}]");
+                    }
+                }
+                if (action != null && !action.isEmpty() && action.equals("retrieve_interviews")) {
+                    int count = Integer.valueOf(req.getParameter("count"));
+                    int id = Integer.valueOf(req.getParameter("id"));
+                    String sort = req.getParameter("sort");
+                    if (id > 0 && (sort != null && !sort.isEmpty()) && count > 0) {
+                        try {
+                            Model m = new Model();
+                            String response = m.retrieve_interviews(count, id, sort);
+                            resp.getWriter().print(response);
+                        } catch (Exception e) {
+                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database:" + e.getMessage() + "\"}]");
+                        }
+                    }
+                    else if (id > 0 && count > 0) {
+                        try {
+                            Model m = new Model();
+                            String response = m.retrieve_interviews(count, id);
+                            resp.getWriter().print(response);
+                        } catch (Exception e) {
+                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database:" + e.getMessage() + "\"}]");
+                        }
+                    }
+                    else{
+                        resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid parameter list!\"}]");
+                    }
+                }
+                else {
+                    resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid URL!\"}]");
+                }
+            }
+            else{
+                resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid API KEY\"}]");
+            }
+        }
+        else{
+            resp.getWriter().print("Welcome to Developer Center!");
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+    throws ServletException, IOException{
+        Map<String, String> dataMap = Utils.getParameterMap(req);
+
+        String api_key = dataMap.get("api_key");
+        if(api_key != null && !api_key.isEmpty()) {
+            resp.setHeader("Content-Type", "application/json;charset=utf-8");
+            if(api_key.equals(this.API_KEY)) {
+                String action = dataMap.get("action");
+                if (action != null && !action.isEmpty() && action.equals("create_developer")) {
+                    String email = dataMap.get("email");
+                    String plang = dataMap.get("plang");
+                    String lang = dataMap.get("lang");
 
                     if ((email != null && !email.isEmpty()) && (plang != null && !plang.isEmpty()) && (lang != null && !lang.isEmpty())) {
                         try {
@@ -27,12 +120,81 @@ public class App extends HttpServlet {
                             String response = m.create_developer(email, plang, lang);
                             resp.getWriter().print(response);
                         } catch (Exception e) {
-                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database: "+e.getMessage()+"\"}]");
+                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database\"}]");
                         }
                     } else {
                         resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid parameter list!\"}]");
                     }
-                } else {
+                }
+                else if (action != null && !action.isEmpty() && action.equals("add_interview")) {
+                    int id = Integer.valueOf(dataMap.get("id"));
+                    int score = Integer.valueOf(dataMap.get("score"));
+                    String comment = dataMap.get("comment");
+
+                    if (id>0 && score>-1) {
+                        try {
+                            Model m = new Model();
+                            String response = m.add_interview(id, score, comment);
+                            resp.getWriter().print(response);
+                        } catch (Exception e) {
+                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database\"}]");
+                        }
+                    } else {
+                        resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid parameter list!\"}]");
+                    }
+                }
+                else {
+                    resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid URL!\"}]");
+                }
+            }
+            else{
+                resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid API KEY\"}]");
+            }
+        }
+        else{
+            resp.getWriter().print("Welcome to Developer Center!");
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException{
+        Map<String, String> dataMap = Utils.getParameterMap(req);
+
+        String api_key = dataMap.get("api_key");
+        if(api_key != null && !api_key.isEmpty()) {
+            resp.setHeader("Content-Type", "application/json;charset=utf-8");
+            if(api_key.equals(this.API_KEY)) {
+                String action = dataMap.get("action");
+                if (action != null && !action.isEmpty() && action.equals("delete_developer")) {
+                    int id = Integer.valueOf(dataMap.get("id"));
+                    if (id > 0) {
+                        try {
+                            Model m = new Model();
+                            String response = m.delete_developer(id);
+                            resp.getWriter().print(response);
+                        } catch (Exception e) {
+                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database\"}]");
+                        }
+                    } else {
+                        resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid parameter list!\"}]");
+                    }
+                }
+                else if (action != null && !action.isEmpty() && action.equals("delete_interview")) {
+                    int id = Integer.valueOf(dataMap.get("id"));
+                    if (id > 0) {
+                        try {
+                            Model m = new Model();
+                            String response = m.delete_interview(id);
+                            resp.getWriter().print(response);
+                        } catch (Exception e) {
+                            resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"System error While connecting Database\"}]");
+                        }
+                    } else {
+                        resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid parameter list!\"}]");
+                    }
+                }
+                else{
                     resp.getWriter().print("[{\"status\":\"failed\",\"error\":\"Invalid URL!\"}]");
                 }
             }
--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -12,15 +12,17 @@ public class Model {
     private Statement statement = null;
     private PreparedStatement preparedStatement = null;
     private ResultSet resultSet = null;
-    private String DBName = "heroku_be826f701685add";
-    private String DBUser = "b363f5860f72e3";
-    private String DBPassword = "94e5fe61";
+    private String DBName = "devcenter";
+    private String DBUser = "root";
+    private String DBPassword = "";
+    //private String DBName = "heroku_be826f701685add";
+    //private String DBUser = "b363f5860f72e3";
+    //private String DBPassword = "94e5fe61";
 
     public Model() throws Exception {
         try {
-            //Class.forName("com.mysql.jdbc.Driver");
-            connect = DriverManager
-                    .getConnection("jdbc:mysql://b363f5860f72e3:94e5fe61@us-cdbr-iron-east-01.cleardb.net/heroku_be826f701685add?reconnect=true");
+            connect = DriverManager.getConnection("jdbc:mysql://b363f5860f72e3:94e5fe61@us-cdbr-iron-east-01.cleardb.net/heroku_be826f701685add?reconnect=true");
+            //connect = DriverManager.getConnection("jdbc:mysql://localhost/"+DBName+"?user="+this.DBUser+"&password="+this.DBPassword);
         } catch (Exception e) {
             throw e;
         }
@@ -29,50 +31,173 @@ public class Model {
     public String create_developer(String email, String plang, String lang) throws SQLException {
         //Insert on developers table and retrieve dev_id;
         preparedStatement = this.connect
-                .prepareStatement("INSERT INTO  "+this.DBName+".developers VALUES (default, ?, default, default)");
+                .prepareStatement("INSERT INTO developers VALUES (default, ?, default, default)");
         preparedStatement.setString(1, email);
         preparedStatement.executeUpdate();
 
         preparedStatement = this.connect
-                .prepareStatement("SELECT last_insert_id() AS last_id FROM "+this.DBName+".developers");
+                .prepareStatement("SELECT last_insert_id() AS last_id FROM developers");
         resultSet = preparedStatement.executeQuery();
         resultSet.next();
         String dev_id = resultSet.getString("last_id");
 
         //Insert on programming languages table and retrieving plang_id;
         preparedStatement = this.connect
-                .prepareStatement("INSERT INTO "+this.DBName+".programming_languages VALUES (default, ?, ?)");
+                .prepareStatement("INSERT INTO programming_languages VALUES (default, ?, ?)");
         preparedStatement.setString(1, dev_id);
         preparedStatement.setString(2, plang);
         preparedStatement.executeUpdate();
 
         preparedStatement = this.connect
-                .prepareStatement("SELECT last_insert_id() AS last_id FROM "+this.DBName+".programming_languages");
+                .prepareStatement("SELECT last_insert_id() AS last_id FROM programming_languages");
         resultSet = preparedStatement.executeQuery();
         resultSet.next();
         String plang_id = resultSet.getString("last_id");
 
         //Insert on languages table and retrieving lang_id;
         preparedStatement = this.connect
-                .prepareStatement("INSERT INTO  "+this.DBName+".languages VALUES (default, ?, ?)");
+                .prepareStatement("INSERT INTO languages VALUES (default, ?, ?)");
         preparedStatement.setString(1, dev_id);
         preparedStatement.setString(2, lang);
         preparedStatement.executeUpdate();
 
         preparedStatement = this.connect
-                .prepareStatement("SELECT last_insert_id() AS last_id FROM "+this.DBName+".languages");
+                .prepareStatement("SELECT last_insert_id() AS last_id FROM languages");
         resultSet = preparedStatement.executeQuery();
         resultSet.next();
         String lang_id = resultSet.getString("last_id");
 
         //Update dating developers table
         preparedStatement = this.connect
-                .prepareStatement("UPDATE  "+this.DBName+".developers SET plang_id=?, lang_id=? WHERE id="+dev_id);
+                .prepareStatement("UPDATE developers SET plang_id=?, lang_id=? WHERE id="+dev_id);
         preparedStatement.setString(1, plang_id);
         preparedStatement.setString(2, lang_id);
         preparedStatement.executeUpdate();
 
         return "[{\"status\":\"success\",\"developer\":\""+email+"\",\"programming_language\":\""+plang+"\",\"language\":\""+lang+"\"}]";
+    }
+
+
+    public String delete_developer(int id) throws SQLException {
+        preparedStatement = this.connect
+                .prepareStatement("DELETE FROM developers WHERE id = ?");
+        preparedStatement.setInt(1, id);
+        int deleted = preparedStatement.executeUpdate();
+
+        if(deleted == 0) return "[{\"status\":\"failed\",\"error\":\"No developer found to deleted.\"}]";
+        else return "[{\"status\":\"success\",\"message\":\"Specified developer is deleted.\"}]";
+    }
+
+    public String add_interview(int dev_id, int score, String comment) throws SQLException {
+        preparedStatement = this.connect
+                .prepareStatement("INSERT INTO interviews VALUES(default,?,?,?)");
+        preparedStatement.setInt(1, dev_id);
+        preparedStatement.setInt(2, score);
+        preparedStatement.setString(3, comment);
+        int added = preparedStatement.executeUpdate();
+
+        if(added == 0) return "[{\"status\":\"failed\",\"error\":\"System error!\"}]";
+        else return "[{\"status\":\"success\",\"message\":\"Interview successfully added.\"}]";
+    }
+
+    public String delete_interview(int interview_id) throws SQLException {
+        preparedStatement = this.connect
+                .prepareStatement("DELETE FROM interviews WHERE interview_id = ?");
+        preparedStatement.setInt(1, interview_id);
+        int deleted = preparedStatement.executeUpdate();
+
+        if(deleted == 0) return "[{\"status\":\"failed\",\"error\":\"No interview found to deleted.\"}]";
+        else return "[{\"status\":\"success\",\"message\":\"Specified interview is deleted.\"}]";
+    }
+
+    public String retrieve_interviews(int count, int dev_id) throws SQLException {
+        preparedStatement = this.connect
+                .prepareStatement("SELECT d.id AS ID, d.email AS Email, pl.name AS ProgrammingLanguage, l.code AS LanguageCode FROM developers d INNER JOIN programming_languages pl ON d.plang_id = pl.plang_id INNER JOIN languages l ON d.lang_id = l.lang_id WHERE d.id = "+dev_id);
+        resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        String JSON = "[{\"Developer\":[{\"ID\":\""+resultSet.getString("ID")+"\",\"Email\":\""+resultSet.getString("Email")+"\",\"Programming Language\":\""+resultSet.getString("ProgrammingLanguage")+"\",\"Language\":\""+resultSet.getString("LanguageCode")+"\"}],";
+
+        preparedStatement = this.connect
+                .prepareStatement("SELECT * FROM interviews WHERE id = "+dev_id+" LIMIT "+count);
+        resultSet = preparedStatement.executeQuery();
+        JSON += "\"Interviews\":[";
+        while(resultSet.next()){
+            JSON += "{\"ID\":\""+resultSet.getString("interview_id")+"\",\"Score\":\""+resultSet.getString("score")+"\",\"Comment\":\""+resultSet.getString("comment")+"\"},";
+        }
+        JSON = JSON.substring(0, JSON.length()-1)+"]}]";
+        if(JSON.length() < 40) return "[{\"status\":\"failed\",\"error\":\"No developer found\"}]";
+        else return JSON;
+    }
+
+    public String retrieve_interviews(int count, int dev_id, String sort) throws SQLException {
+        preparedStatement = this.connect
+                .prepareStatement("SELECT d.id AS ID, d.email AS Email, pl.name AS ProgrammingLanguage, l.code AS LanguageCode FROM developers d INNER JOIN programming_languages pl ON d.plang_id = pl.plang_id INNER JOIN languages l ON d.lang_id = l.lang_id WHERE d.id = "+dev_id);
+        resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        String JSON = "[{\"Developer\":[{\"ID\":\""+resultSet.getString("ID")+"\",\"Email\":\""+resultSet.getString("Email")+"\",\"Programming Language\":\""+resultSet.getString("ProgrammingLanguage")+"\",\"Language\":\""+resultSet.getString("LanguageCode")+"\"}],";
+
+        preparedStatement = this.connect
+                .prepareStatement("SELECT * FROM interviews WHERE id = "+dev_id+ " ORDER BY score "+sort+" LIMIT "+count);
+        resultSet = preparedStatement.executeQuery();
+        JSON += "\"Interviews\":[";
+        while(resultSet.next()){
+            JSON += "{\"ID\":\""+resultSet.getString("interview_id")+"\",\"Score\":\""+resultSet.getString("score")+"\",\"Comment\":\""+resultSet.getString("comment")+"\"},";
+        }
+        JSON = JSON.substring(0, JSON.length()-1)+"]}]";
+        if(JSON.length() < 40) return "[{\"status\":\"failed\",\"error\":\"No developer found\"}]";
+        else return JSON;
+    }
+
+    public String retrieve_developer(int count) throws SQLException {
+        preparedStatement = this.connect
+                .prepareStatement("SELECT d.id AS ID, d.email AS Email, pl.name AS ProgrammingLanguage, l.code AS LanguageCode FROM developers d INNER JOIN programming_languages pl ON d.plang_id = pl.plang_id INNER JOIN languages l ON d.lang_id = l.lang_id LIMIT "+count);
+        resultSet = preparedStatement.executeQuery();
+        String JSON = "[";
+        while(resultSet.next()){
+            JSON += "{\"ID\":\""+resultSet.getString("ID")+"\",\"Email\":\""+resultSet.getString("Email")+"\",\"Programming Language\":\""+resultSet.getString("ProgrammingLanguage")+"\",\"Language\":\""+resultSet.getString("LanguageCode")+"\"},";
+        }
+        JSON = JSON.substring(0, JSON.length()-1)+"]";
+        if(JSON.length() < 5) return "[{\"status\":\"failed\",\"error\":\"No developer found\"}]";
+        else return JSON;
+    }
+
+    public String search_lang_developer(String lang, int count) throws SQLException {
+        preparedStatement = this.connect
+                .prepareStatement("SELECT d.id AS ID, d.email AS Email, pl.name AS ProgrammingLanguage, l.code AS LanguageCode FROM developers d INNER JOIN programming_languages pl ON d.plang_id = pl.plang_id INNER JOIN languages l ON d.lang_id = l.lang_id WHERE l.code LIKE '%"+lang+"%' LIMIT "+count);
+        resultSet = preparedStatement.executeQuery();
+        String JSON = "[";
+        while(resultSet.next()){
+            JSON += "{\"ID\":\""+resultSet.getString("ID")+"\",\"Email\":\""+resultSet.getString("Email")+"\",\"Programming Language\":\""+resultSet.getString("ProgrammingLanguage")+"\",\"Language\":\""+resultSet.getString("LanguageCode")+"\"},";
+        }
+        JSON = JSON.substring(0, JSON.length()-1)+"]";
+        if(JSON.length() < 5) return "[{\"status\":\"failed\",\"error\":\"No developer found\"}]";
+        else return JSON;
+    }
+
+    public String search_plang_developer(String plang, int count) throws SQLException {
+        preparedStatement = this.connect
+                .prepareStatement("SELECT d.id AS ID, d.email AS Email, pl.name AS ProgrammingLanguage, l.code AS LanguageCode FROM developers d INNER JOIN programming_languages pl ON d.plang_id = pl.plang_id INNER JOIN languages l ON d.lang_id = l.lang_id WHERE pl.name LIKE '%"+plang+"%' LIMIT "+count);
+        resultSet = preparedStatement.executeQuery();
+        String JSON = "[";
+        while(resultSet.next()){
+            JSON += "{\"ID\":\""+resultSet.getString("ID")+"\",\"Email\":\""+resultSet.getString("Email")+"\",\"Programming Language\":\""+resultSet.getString("ProgrammingLanguage")+"\",\"Language\":\""+resultSet.getString("LanguageCode")+"\"},";
+        }
+        JSON = JSON.substring(0, JSON.length()-1)+"]";
+        if(JSON.length() < 5) return "[{\"status\":\"failed\",\"error\":\"No developer found\"}]";
+        else return JSON;
+    }
+
+    public String search_developer(String lang, String plang, int count) throws SQLException {
+        preparedStatement = this.connect
+                .prepareStatement("SELECT d.id AS ID, d.email AS Email, pl.name AS ProgrammingLanguage, l.code AS LanguageCode FROM developers d INNER JOIN programming_languages pl ON d.plang_id = pl.plang_id INNER JOIN languages l ON d.lang_id = l.lang_id WHERE l.code LIKE '%"+lang+"%' AND pl.name LIKE '%"+plang+"%' LIMIT "+count);
+        resultSet = preparedStatement.executeQuery();
+        String JSON = "[";
+        while(resultSet.next()){
+            JSON += "{\"ID\":\""+resultSet.getString("ID")+"\",\"Email\":\""+resultSet.getString("Email")+"\",\"Programming Language\":\""+resultSet.getString("ProgrammingLanguage")+"\",\"Language\":\""+resultSet.getString("LanguageCode")+"\"},";
+        }
+        JSON = JSON.substring(0, JSON.length()-1)+"]";
+        if(JSON.length() < 5) return "[{\"status\":\"failed\",\"error\":\"No developer found\"}]";
+        else return JSON;
     }
 
     public void readDataBase() throws Exception {
